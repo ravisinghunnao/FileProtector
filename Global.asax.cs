b4c9626a100b3cf2f6043fd6724dbc37eab61605
Ravi Singh <ravi.singh.unnao@gmail.com>
2020-03-30 22:08:31 +0530

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplication2
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        internal protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            // Get objects.
+            HttpContext context = base.Context;
+
+            context.Request.Headers.Add("FileServerHeader", "a secure header");
+            // Complete.
+           
+        }
+    }
+}
